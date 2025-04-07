471a7c2d8f0d66e57e66d721944489dcc8916c24
Cascade Code <fahim.real.x@gmail.com>
2025-04-07 08:23:33 +0600

    chore: add tailwindcss

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * dental_design functions and definitions
  *
@@ -7,9 +8,9 @@
  * @package dental_design
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (! defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,17 +20,18 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function dental_design_setup() {
+function dental_design_setup()
+{
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on dental_design, use a find and replace
 		* to change 'dental_design' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'dental_design', get_template_directory() . '/languages' );
+	load_theme_textdomain('dental_design', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -37,19 +39,19 @@ function dental_design_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'dental_design' ),
+			'menu-1' => esc_html__('Primary', 'dental_design'),
 		)
 	);
 
@@ -83,7 +85,7 @@ function dental_design_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -100,7 +102,7 @@ function dental_design_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'dental_design_setup' );
+add_action('after_setup_theme', 'dental_design_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,22 +111,24 @@ add_action( 'after_setup_theme', 'dental_design_setup' );
  *
  * @global int $content_width
  */
-function dental_design_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'dental_design_content_width', 640 );
+function dental_design_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('dental_design_content_width', 640);
 }
-add_action( 'after_setup_theme', 'dental_design_content_width', 0 );
+add_action('after_setup_theme', 'dental_design_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function dental_design_widgets_init() {
+function dental_design_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'dental_design' ),
+			'name'          => esc_html__('Sidebar', 'dental_design'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'dental_design' ),
+			'description'   => esc_html__('Add widgets here.', 'dental_design'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -132,22 +136,32 @@ function dental_design_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'dental_design_widgets_init' );
+add_action('widgets_init', 'dental_design_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function dental_design_scripts() {
-	wp_enqueue_style( 'dental_design-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'dental_design-style', 'rtl', 'replace' );
+function dental_design_scripts()
+{
+	wp_enqueue_style('dental_design-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('dental_design-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'dental_design-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_style(
+		'dental_design-tw',
+		get_template_directory_uri() . '/tw.css',
+		array('dental_design-style'), // Depends on main style
+		filemtime(get_template_directory() . '/tw.css')
+	);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+
+	wp_enqueue_script('dental_design-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
+
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'dental_design_scripts' );
+
+add_action('wp_enqueue_scripts', 'dental_design_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -172,7 +186,6 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
