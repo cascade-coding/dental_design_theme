ed1381574913cb4e40c90eab4114bc987472e9f1
Cascade Code <fahim.real.x@gmail.com>
2025-05-24 11:44:06 +0600

    chore improve offer slider

--- front-page.php
+++ front-page.php
@@ -344,30 +344,36 @@ get_header();
 <!-- offer section -->
 
 <section>
-    <div>
-
+    <div class="pt-6">
 
 
         <!-- Swiper -->
         <div class="swiper swiper-home-middle bg-accent-500 !h-[492px] w-full relative">
             <div class="swiper-wrapper relative">
 
-                <!-- <div class="swiper-slide relative">
+
+                <div class="swiper-slide relative">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home-slider-middle-1.jpg" alt="" srcset="">
-                    <div class="absolute left-1/2 transform -translate-x-1/2 container bottom-0 right-4 min-h-36 hidden lg:block ">
-                        <div class="absolute right-4 bg-primary-100/50 py-4 px-6 pr-10 w-xl">
-                            <p class="font-primary text-4xl leading-relaxed tracking-wide font-bold text-left max-w-4xl text-neutral-900">Expert Care. Tailored Smiles.</p>
+                    <div class="absolute inset-0 bg-black opacity-40 z-10"></div>
+
+                    <div class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 container min-h-36 z-50">
+                        <div class="flex items-center justify-center flex-col h-full">
+                            <h3 class="text-4xl font-primary font-bold text-neutral-50 tracking-wide leading-relaxed">Your Smile, Reimagined.</h3>
+                            <p class="text-neutral-50 font-semibold mt-2.5">Explore cutting-edge cosmetic treatments that bring out the best in your smile.</p>
+                            <a href="#" class="!no-underline bg-secondary-500 px-6 py-2 rounded-full text-neutral-50 mt-5 text-lg font-medium hover:bg-secondary-600 transition-all">Discover Cosmetic Solutions</a>
                         </div>
                     </div>
-                </div> -->
+                </div>
 
                 <div class="swiper-slide relative">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home-slider-middle-2.jpg" alt="" srcset="">
-                    <div class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 container min-h-36">
+                    <div class="absolute inset-0 bg-black opacity-40 z-10"></div>
+
+                    <div class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 container min-h-36 z-50">
                         <div class="flex items-center justify-center flex-col h-full">
                             <h3 class="text-4xl font-primary font-bold text-neutral-50 tracking-wide leading-relaxed">Gentle Dentistry & Personalized Care.</h3>
-                            <p class="text-neutral-50 font-semibold mt-2.5">Our private dental office can give you and your family <br> the personalized experience you deserve!</p>
-                            <a href="#" class="!no-underline bg-secondary-500 px-6 py-2 rounded-full text-neutral-50 mt-5 text-lg font-medium hover:bg-secondary-600 transition-all">Read More About Family Dentistry</a>
+                            <p class="text-neutral-50 font-semibold mt-2.5">We use state-of-the-art technology for faster, safer, and more precise care.</p>
+                            <a href="#" class="!no-underline bg-secondary-500 px-6 py-2 rounded-full text-neutral-50 mt-5 text-lg font-medium hover:bg-secondary-600 transition-all">Learn About Our Technology</a>
                         </div>
                     </div>
                 </div>
